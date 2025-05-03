6d5ffb91d014a1c4c4f78cd125693b0c5b4223aa
ernestoacosta75 <ernestoacostacuba@yahoo.com.mx>
2025-05-03 13:16:28 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Header from "./components/Header/Header";
+import { CORE_CONCEPTS } from './data.js';
+function CoreConcept({image, title, description}) {
+  return (
+    <li>
+      <img src={image} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
